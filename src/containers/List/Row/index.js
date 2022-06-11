2fa5bd27cc343d0743544c9662ea79d614aee2ef
Apple <nishanth.saka@metronomic.in>
2022-06-11 12:25:39 +0530

    Final Commit - Code Cleanup

--- src/containers/List/Row/index.js
+++ src/containers/List/Row/index.js
@@ -4,18 +4,6 @@ import { Animated, TouchableOpacity } from 'react-native'
 import ListRowStyles from './ListRowStyles';
 import ListRowGif from './RowGif';
 
-
-const propsAreEqual = (preItem, nextItem) => {
-  
-  // console.log(`ListRow`);
-  // console.log(`preItem?.item?.id`, preItem?.item?.id);
-  // console.log(`nextItem?.item?.id`, nextItem?.item?.id);
-  // console.log(``);
-
-    return preItem?.item?.id === nextItem?.item?.id;
-    // return true;
-  }
-
 function ListRow({item}) {
   const {id, images : {original_still, downsized_small, original, downsized_medium, fixed_width_small_still}, user} = item;
 
